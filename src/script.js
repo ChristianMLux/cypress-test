29b62cbce9315a9372548d25473258c3e9ec1c8d
ChristianMLux <christian.m.lux@gmail.com>
2021-07-29 15:37:26 +0200

    test#2

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 const input = document.querySelector('#input');
 const addButton = document.querySelector('#add-button');
-const list = document.querySelector('#list')
+const list = document.querySelector('#list');
 
-addButton.addEventListener('click', addTodo)
+addButton.addEventListener('click', addTodo);
 
 function addTodo() {
   const newTodo = input.value;
